c8904ccecc66e2012a999944e40ab2afc7f00b13
arsvechkarev <arsvechkarev@gmail.com>
2019-12-29 14:03:41 +0300

    Adding first version of compound toolbar

--- views/src/main/java/views/TheToolbar.kt
+++ views/src/main/java/views/TheToolbar.kt
@@ -2,10 +2,34 @@ package views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class TheToolbar @JvmOverloads constructor(
   context: Context,
   attrs: AttributeSet? = null,
   defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr)
+) : LinearLayout(context, attrs, defStyleAttr) {
+  
+  private var hasSearch = false
+  private var title = ""
+  
+  private val textTitle by lazy { findViewById<TextView>(R.id.textTitle) }
+  private val imageBack by lazy { findViewById<ImageView>(R.id.imageBack) }
+  private val imageSearch by lazy { findViewById<ImageView>(R.id.imageSearch) }
+  
+  init {
+    inflate(context, R.layout.the_toolbar, this)
+    val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.TheToolbar, 0, 0)
+    try {
+      title = typedArray.getString(R.styleable.TheToolbar_title) ?: ""
+      hasSearch = typedArray.getBoolean(R.styleable.TheToolbar_hasSearch, false)
+    } finally {
+      typedArray.recycle()
+    }
+    textTitle.text = title
+  }
+  
+  
+}
